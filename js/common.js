fb5e5429e8b119a3796ae7bb940e08bdb1d313c5
dj107 <dheerajghosh007@gmail.com>
2021-05-20 16:15:13 +0530

    schedule

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 jQuery(function ($) {
 
-    'use strict';
-	
+  'use strict';
+
 // =============================================
 // BEGIN THEME SCRIPTS
 // =============================================
@@ -12,34 +12,34 @@ new WOW().init();
 
 //Parallax	
 jQuery(window).bind('load', function () {
-	parallaxInit();						  
+parallaxInit();						  
 });
 function parallaxInit() {
-    jQuery('.parallax').each(function(){
-        jQuery(this).parallax("30%", 0.1);
-    });
+  jQuery('.parallax').each(function(){
+      jQuery(this).parallax("30%", 0.1);
+  });
 }
 
 
 
 // Somth page scroll
 $(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top -40
-        }, 1000);
-        return false;
-      }
+$('a[href*=#]:not([href*=-])').click(function() {
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('html,body').animate({
+        scrollTop: target.offset().top -40
+      }, 1000);
+      return false;
     }
-  });
+  }
+});
 });
 
 // =============================================
 // END THEME SCRIPTS
 // =============================================
-	
+
 });
